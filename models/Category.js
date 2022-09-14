0ec79d61ea3e3b13aa1034bb4613340d7960e611
PierTwo <105090994+PierTwo@users.noreply.github.com>
2022-09-14 07:59:46 -0500

    Defined columns for category model

--- models/Category.js
+++ models/Category.js
@@ -1,19 +1,30 @@
-const { Model, DataTypes } = require('sequelize');
-
-const sequelize = require('../config/connection.js');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection.js");
 
 class Category extends Model {}
 
+// Initializes a Category Model
 Category.init(
   {
-    // define columns
+    // Column for category ids to be used as the primary keys
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // Column for product category names
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: "category",
   }
 );
 
