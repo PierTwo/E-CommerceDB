e47e30212eb7e7bd4dfb6822d89abf693b40f0a5
PierTwo <105090994+PierTwo@users.noreply.github.com>
2022-09-15 13:20:57 -0500

    Added get routes for finding all tags and by id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,27 +1,55 @@
-const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Tag, Product, ProductTag } = require("../../models");
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+// Get route to find all tags
+router.get("/", async (req, res) => {
+  try {
+    // Variable to await the find all method on the tag model
+    const tagData = await Tag.findAll({
+      // Include the products associated with each category
+      include: [{ model: Product }],
+    });
+
+    // Send a response status of 200 with the results in JSON
+    res.status(200).json(tagData);
+
+    // Catch for errors
+  } catch (err) {
+    // Send a response status of 500 and the error in JSON
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+// Get route to find one tag by its id
+router.get("/:id", async (req, res) => {
+  try {
+    // Variable to await the find all method on the tag model
+    const tagData = await Tag.findByPk(req.params.id, {
+      // Include the products associated with each category
+      include: [{ model: Product }],
+    });
+
+    // Send a response status of 200 with the results in JSON
+    res.status(200).json(tagData);
+
+    // Catch for errors
+  } catch (err) {
+    // Send a response status of 500 and the error in JSON
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // create a new tag
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete on tag by its `id` value
 });
 
